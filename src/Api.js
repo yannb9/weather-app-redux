fbdee4d97ca763d2edaf27cf7dc9f1a7906f1ddb
Yann Bohbot <yannbohbot@gmail.com>
2020-10-01 03:31:30 +0300

    make it better4

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 export const Api = {
     // key: '2dwASrdUOJbjmW5aso7RSvYRXbxteqGD'
     // key: '8Fl1zAkdqovyzwu94s2KGEz6qMn6u29x'
-    // key: 'y66swWoKjE5c2bGPzJqGBAfCZVWfXAUH',
-    key: 'G1UWRm1FvnyXYozqRz8pqKHouqeODDQE'
+    key: 'dSNILD0NmOT17rhNdYdcsrbM6knUlYQK'
+    // key: 'G1UWRm1FvnyXYozqRz8pqKHouqeODDQE'
 }
 
